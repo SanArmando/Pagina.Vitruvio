a4f7547bc07df360e78758ecda63e542a06b6023
San Armando <traspasaluxi@gmail.com>
2025-02-25 23:51:21 -0500

    2.2

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
         lastScrollTop = scrollTop;
         });
     });
-});
+
+    
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.querySelector(".menu-icono");
     const navbar = document.querySelector(".navbar");
@@ -21,3 +22,20 @@ document.addEventListener("DOMContentLoaded", function() {
     menuIcon.addEventListener("click", function() {
         navbar.classList.toggle("active");
     });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        let lastScrollTop = 0;
+        const menu = document.querySelector(".menu");
+    
+        window.addEventListener("scroll", function() {
+            let scrollTop = window.scrollY;
+    
+            if (scrollTop > lastScrollTop) {
+                menu.style.top = "-80px"; // Oculta el menú
+            } else {
+                menu.style.top = "0"; // Muestra el menú
+            }
+            lastScrollTop = scrollTop;
+        });
+    });
+    
